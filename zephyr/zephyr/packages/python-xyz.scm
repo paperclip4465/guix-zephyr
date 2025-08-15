034733b981be485649a7a64d5fabcdaf6256a139
Mitchell Schmeisser <mitchellschmeisser@librem.one>
2025-08-15 00:28:34 -0400

    Removed python-cbor2
    This is now packaged upstream

--- zephyr/zephyr/packages/python-xyz.scm
+++ zephyr/zephyr/packages/python-xyz.scm
@@ -14,38 +14,17 @@
     (name "python-setuptools-scm")
     (version "7.0.5")
     (source (origin
-	      (method url-fetch)
-	      (uri (pypi-uri "setuptools_scm" version))
-	      (sha256
-	       (base32
-		"0i28zghzdzzkm9w8rrjwphggkfs58nh6xnqsjhmqjvqxfypi67h3"))))
+              (method url-fetch)
+              (uri (pypi-uri "setuptools_scm" version))
+              (sha256
+               (base32
+                "0i28zghzdzzkm9w8rrjwphggkfs58nh6xnqsjhmqjvqxfypi67h3"))))
     (build-system python-build-system)
     (propagated-inputs (list python-importlib-metadata python-packaging
-			     python-setuptools python-tomli
-			     python-typing-extensions))
+                             python-setuptools python-tomli
+                             python-typing-extensions))
     (native-inputs (list python-pytest python-virtualenv))
     (home-page "https://github.com/pypa/setuptools_scm/")
     (synopsis "the blessed package to manage your versions by scm tags")
     (description "the blessed package to manage your versions by scm tags")
-    (license license:expat)))
-
-(define-public python-cbor2
-  (package
-    (name "python-cbor2")
-    (version "5.4.3")
-    (source (origin
-	      (method url-fetch)
-	      (uri (pypi-uri "cbor2" version))
-	      (sha256
-	       (base32
-		"1hj6wllgwdsj2jc88fsxk585adsg930z6j79mwr41vbcxv2n7f32"))))
-    (build-system python-build-system)
-    (propagated-inputs
-     (list python-wheel))
-    (native-inputs (list python-setuptools-scm-7.0.5
-			 python-pytest
-			 python-pytest-cov))
-    (home-page "")
-    (synopsis "CBOR (de)serializer with extensive tag support")
-    (description "CBOR (de)serializer with extensive tag support")
     (license license:expat)))
