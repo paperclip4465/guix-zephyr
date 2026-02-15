3cc6d9da585cbb030f93d7d7aa109c39f94f8919
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2026-02-15 02:06:27 +0100

    Add license/copyright notices in files.
    GNU Guix uses similar copyright notices. So adding them here would
    make it easier to upstream all that work in Guix proper.
    
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- zephyr/zephyr/build-system/zephyr.scm
+++ zephyr/zephyr/build-system/zephyr.scm
@@ -1,3 +1,22 @@
+;;; Guix Zephyr --- Functional package management for ZephyrRTOS
+;;; Copyright © 2023,2025 Mitchell Schmeisser <mitchellschmeisser@librem.one>
+;;; Copyright © 2023 Herman Rimm <herman@rimm.ee>
+;;;
+;;; This file is NOT part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
 (define-module (zephyr build-system zephyr)
   #:use-module (guix packages)
   #:use-module (guix discovery)
